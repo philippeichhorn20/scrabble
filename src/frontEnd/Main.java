db731fd176e039b115a7b01233005feab57f488f
Michal Kolinski <mkolinsk@mail.uni-mannheim.de>
2021-03-28 12:14:18 +0200

    #12 Updated the Main class so its more compatible with the rest of the project. Hopefully now you can pull this and it will work properly. lmk

--- src/frontEnd/Main.java
+++ src/frontEnd/Main.java
@@ -14,22 +14,25 @@ public class Main extends Application {
     private static Stage stg;
 
     /*
-        This is the first scene the user sees when starting the application
-        Here they can choose to either create a new profile or to log into an already existing one
+    *    This is the first scene the user sees when starting the application
+    *   Here they can choose to either create a new profile or to log into an already existing one
      */
     @Override
     public void start(Stage primaryStage) throws Exception{
         stg = primaryStage;
         primaryStage.setResizable(false);
-        Parent root = FXMLLoader.load(getClass().getResource("resources/startingMenu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("screens/startingMenu.fxml"));
         primaryStage.setTitle("Scrabble Application");
         primaryStage.setScene(new Scene(root,1080,720));
         primaryStage.show();
     }
 
+    /*
+    *   This method is called whenever the user uses a button to go into another screen scene
+    */
     public void changeScene(String fxml) throws IOException {
         Parent pane = FXMLLoader.load(getClass().getResource(fxml));
-        stg.getScene().setRoot(pane);3
+        stg.getScene().setRoot(pane);
     }
 
     public static void main(String[] args) {
