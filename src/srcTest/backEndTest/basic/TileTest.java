b1a77532a985868ac9565a73825b9f051e7e662a
Nils Holger Bernhard Schaefer <nilschae@mail.uni-mannheim.de>
2021-03-31 13:46:05 +0200

    #25 created a JUnit test case for the Tile.java class

---
+++ src/srcTest/backEndTest/basic/TileTest.java
@@ -0,0 +1,14 @@
+package srcTest.backEndTest.basic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TileTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
