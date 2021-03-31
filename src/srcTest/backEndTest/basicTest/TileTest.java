09e27d569e390fe88834befa8527b0bee1ce4ef0
Nils Holger Bernhard Schaefer <nilschae@mail.uni-mannheim.de>
2021-03-31 16:55:23 +0200

    #24 added tests to test the tile class

--- src/srcTest/backEndTest/basicTest/TileTest.java
+++ src/srcTest/backEndTest/basicTest/TileTest.java
@@ -4,11 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import backEnd.basic.Tile;
+import backEnd.basic.Tile.Tilestatus;
+
 class TileTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		Tile testTile = new Tile('A', 1);
+		assertEquals(testTile.getLetter(), 'A');
+		assertEquals(testTile.getValue(), 1);
+		assertEquals(testTile.getStatus(), Tilestatus.INBAG);
 	}
 
 }
