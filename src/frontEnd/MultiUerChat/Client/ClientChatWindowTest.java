38ff5c677ee09a3d0e1e1b89cd9066a0470c61f7
Viktoria Milenova Ivanova <vivanova@mail.uni-mannheim.de>
2021-04-10 07:46:30 +0200

    #27 Chat Client

---
+++ src/frontEnd/MultiUerChat/Client/ClientChatWindowTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package frontEnd.MultiUerChat.Client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author vInvanova
+ *
+ */
+class ClientChatWindowTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
