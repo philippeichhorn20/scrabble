5301beb447d6f75dab338a646244580d1b38909d
peichhor <peichhor@mail.uni-mannheim.de>
2021-04-17 11:15:55 +0200

    #16 Word check
    Signed-off-by: peichhor <peichhor@mail.uni-mannheim.de>

---
+++ src/backend/basic/WordCheckDB.java
@@ -0,0 +1,40 @@
+package backend.basic;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/* @author peichhor
+ * @version 1.0
+ * @description a class which checks, weather a word is valid or not
+ * @param url safes the url to the database
+ * */
+public class WordCheckDB {
+
+  static String url = "jdbc:sqlite:ScrabbleWordsDB";
+
+  /*
+  looks up the given word in the database. If it exists it returns the decsription of the word,
+  if not it returns null
+  The function ignores weather the word is in small or capital letters to reduce errors
+   */
+  static boolean findWord(String word) {
+    try {
+      Class.forName("org.sqlite.JDBC");
+    } catch (final ClassNotFoundException e) {
+      System.out.println(e);
+    }
+    try (Connection conn = DriverManager.getConnection(WordCheckDB.url)) {
+      java.sql.Statement stm = conn.createStatement();
+      word = word.toUpperCase();
+      ResultSet rs = stm
+          .executeQuery("SELECT description FROM words WHERE (word = '" + word + "');");
+      return rs != null;
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    }
+    return false;
+  }
+
+}
