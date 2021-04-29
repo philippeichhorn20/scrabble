6a8ed8d004c0548b0eedb059ae13add2c094cecf
Nils Holger Bernhard Schaefer <nilschae@mail.uni-mannheim.de>
2021-04-29 18:10:16 +0200

    #15 modified setUp methode and created save and resetScore methode for save and reset Button.

--- src/frontEnd/screens/controllers/ManageProfileController.java
+++ src/frontEnd/screens/controllers/ManageProfileController.java
@@ -1,8 +1,14 @@
 package frontEnd.screens.controllers;
 
+import backend.basic.Profile;
 import frontend.Main;
+import java.util.Optional;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ProgressBar;
@@ -10,33 +16,76 @@ import javafx.scene.input.MouseEvent;
 
 /*
 * @author nilschae
+* @version 1.0
+* @descritption A controller to manage different settings of a profile
 * */
 public class ManageProfileController {
-  @FXML private Button BT_SaveChanges;
+  @FXML private Button BT_Save;
   @FXML private Button BT_ResetScore;
   @FXML private Button BT_DeleteProfile;
   @FXML private Button BT_GoBack;
   @FXML private Label LB_ProfileName;
   @FXML private Label LB_ProfileColor;
-  @FXML private Label LB_TotalPoints;
-  @FXML private Label LB_Level;
-  @FXML private Label LB_EXP;
+  @FXML private Label LB_Wins;
+  @FXML private Label LB_Games;
+  @FXML private Label LB_Points;
   @FXML private TextField TF_ProfileName;
   @FXML private TextField TF_ProfileColor;
-  @FXML private ProgressBar PB_EXP;
+
 
   private boolean setUpDone = false;
+  Profile currentProfile;
 
   public void setUp(MouseEvent e) {
     if (!setUpDone) {
-      String name = frontend.Main.profile.getName();
-      String color = Main.profile.getColor();
+      currentProfile = Main.profile;
+      String name = currentProfile.getName();
+      String color = currentProfile.getColor();
+
       name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+
       TF_ProfileName.setText(name);
       TF_ProfileColor.setText(color);
+
+      LB_Games.setText("Games: " + currentProfile.getGames());
+      LB_Points.setText("Points: " + currentProfile.getPoints());
+      LB_Wins.setText("Wins: " + currentProfile.getWins());
+
       setUpDone = true;
     }
   }
 
+  public void save() {
+    String newName = TF_ProfileName.getText();
+    String newColor = TF_ProfileColor.getText();
+
+    currentProfile.setName(newName, currentProfile.getId());
+    currentProfile.setColor(newColor);
+
+    Alert successfulSaved = new Alert(AlertType.INFORMATION);
+    successfulSaved.setTitle("Successful saved!");
+    successfulSaved.setContentText("Your name and color has been saved as:\n"
+        + "name: " + newName + "color: " + newColor);
+
+  }
+
+  public void resetScore() {
+    Alert areYouSure = new Alert(AlertType.CONFIRMATION);
+    areYouSure.setTitle("Confirm Reset");
+    areYouSure.setContentText("When you continue you will lose all your progress!\n"
+        + "If you want to reset your score please continue");
+
+    Optional<ButtonType> result = areYouSure.showAndWait();
+
+    if(result.get() == ButtonType.OK) {
+      currentProfile.setWins(0,currentProfile.getId());
+      currentProfile.setGames(0,currentProfile.getId());
+      currentProfile.setPoints(0,currentProfile.getId());
+    }
+
+    LB_Games.setText("Games: " + currentProfile.getGames());
+    LB_Points.setText("Points: " + currentProfile.getPoints());
+    LB_Wins.setText("Wins: " + currentProfile.getWins());
+  }
 
 }
