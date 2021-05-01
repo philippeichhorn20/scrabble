7423a99bf412414eab8852ec104f08390b9e8979
Nils Holger Bernhard Schaefer <nilschae@mail.uni-mannheim.de>
2021-05-01 12:28:24 +0200

    #22 Added a basic server which can handle messages. To work ClientProtocol have to be added. Also added ServerSettings which allows to communicate via IPv4. Created shutdown message

--- src/backend/network/messages/ShutDownMessage.java
+++ src/backend/network/messages/ShutDownMessage.java
@@ -1,5 +1,9 @@
 package backend.network.messages;
 
-public class ShutDownMessage {
+public class ShutDownMessage extends Message{
 
+
+  public ShutDownMessage(String from) {
+    super(MessageType.SERVER_SHUTDOWN, from);
+  }
 }
