510872a4cf478b44b6870e0c10c20dfb5de6dc8b
Viktoria Milenova Ivanova <vivanova@mail.uni-mannheim.de>
2021-05-06 14:28:19 +0200

    #31

--- src/frontend/screens/controllers/RuleScreenController.java
+++ src/frontend/screens/controllers/RuleScreenController.java
@@ -1,8 +1,32 @@
 package frontend.screens.controllers;
 
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.w3c.dom.Text;
+
+import frontend.Main;
+import javafx.fxml.FXML;
+import javafx.scene.text.TextFlow;
+
 /*
     TODO:
-    This is an empty controller for the rules screen. Here the user should be able to get to know the rules of the game.
+    This is a controller for the rules screen. Here the user is able to get to know the rules of the game.
+    
+    There is a back button included, that leads to back the game. 
  */
 public class RuleScreenController {
+
+	@FXML private Button backButton;
+
+	public void goBack(ActionEvent e) throws IOException {
+		Main m = new Main();
+		m.changeScene("screens/gameScreen.fxml");
+	}	
+ 
+
+	
 }
