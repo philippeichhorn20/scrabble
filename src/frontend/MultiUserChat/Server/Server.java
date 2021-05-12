b7bb351ce199d3df7bc2bd79e414164ba138ccda
Viktoria Milenova Ivanova <vivanova@mail.uni-mannheim.de>
2021-05-12 18:22:03 +0200

    #34 added music file

--- src/frontend/MultiUserChat/Server/Server.java
+++ src/frontend/MultiUserChat/Server/Server.java
@@ -1,4 +1,4 @@
-package frontEnd.MultiUserChat.Server;
+package frontend.MultiUserChat.Server;
 
 import java.net.DatagramPacket;
 import java.net.InetAddress;
--- src/frontend/screens/controllers/SettingsScreenController.java
+++ src/frontend/screens/controllers/SettingsScreenController.java
@@ -2,9 +2,13 @@ package frontend.screens.controllers;
 
 import frontend.Main;
 import java.io.IOException;
+import java.net.URISyntaxException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 /*
    TODO:
@@ -18,4 +22,15 @@ public class SettingsScreenController {
     Main m = new Main();
     m.changeScene("screens/mainMenu.fxml");
   }
+  
+  public static void main(String[] args){
+	    MediaPlayer player = new MediaPlayer( new Media(uriString));
+	    player.play();
+	    Media media = null;
+	    try {
+	      media = new Media(getClass().getResource("/music/hero.mp3").toURI().toString());
+	    } catch (URISyntaxException e) {
+	      e.printStackTrace();
+	    } 
+	}
 }
