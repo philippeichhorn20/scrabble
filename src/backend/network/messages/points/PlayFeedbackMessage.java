f6cf3873d9543ac692baad382fd63c25d0690c2c
peichhor <peichhor@mail.uni-mannheim.de>
2021-05-15 23:05:08 +0200

    #15 PlayFeedbackMessage
    Signed-off-by: peichhor <peichhor@mail.uni-mannheim.de>

---
+++ src/backend/network/messages/points/PlayFeedbackMessage.java
@@ -0,0 +1,46 @@
+package backend.network.messages.points;
+
+import backend.network.messages.Message;
+import backend.network.messages.MessageType;
+
+/*
+@author peichhor
+ */
+public class PlayFeedbackMessage extends Message {
+
+  String[][] feedback;
+  boolean successfulMove;
+
+  public PlayFeedbackMessage(String from, String[][] feedback, boolean successfulMove) {
+    super(MessageType.GAME_TURN, from);
+    this.feedback = feedback;
+    this.successfulMove = successfulMove;
+  }
+
+  @Override
+  public String getFrom() {
+    return super.getFrom();
+  }
+
+  @Override
+  public void setFrom(String name) {
+    super.setFrom(name);
+  }
+
+  public String[][] getFeedback() {
+    return feedback;
+  }
+
+  public void setFeedback(String[][] feedback) {
+    this.feedback = feedback;
+  }
+
+  @Override
+  public MessageType getMessageType() {
+    return super.getMessageType();
+  }
+
+  public void setSuccessfulMove(boolean successfulMove) {
+    this.successfulMove = successfulMove;
+  }
+}
