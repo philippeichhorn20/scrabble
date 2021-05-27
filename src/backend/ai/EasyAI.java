058d09740d9fbe3ba61d1b05ccbc5fb4221a69fb
Viktoria Milenova Ivanova <vivanova@mail.uni-mannheim.de>
2021-05-27 03:25:24 +0200

    #36 Implemented Easy and Hard AI. Need to add more methods though.

--- src/backend/ai/EasyAI.java
+++ src/backend/ai/EasyAI.java
@@ -1,23 +1,46 @@
 package backend.ai;
 
+import java.util.Random;
 import backend.basic.Player;
 import backend.basic.ScrabbleBoard;
-import backend.basic.Tile;
 
 /*
-@author jawinter
+@author vivanova
 EasyAI uses Brain to find simple possible moves.
  */
 
 public class EasyAI extends Player {
 
   public Brain easyBrain;
-  public Tile[] tilesOnHand = new Tile[7];
+  private Random r = new Random(System.currentTimeMillis());
 
   public EasyAI(String name, String color, Playerstatus status, ScrabbleBoard board) {
     super(name, color, status);
     this.easyBrain = new Brain(board);
-
   }
-
+  
+  public void play() throws Exception {
+	  var moves = easyBrain.getPlayableWords(getRack(), easyBrain.getWordPossibilities());
+	  // Introduce randomness
+	  var random = r.nextInt(moves.size()-1)+1;
+	  // Introduce a counter so we can 
+	  int counter = 0;
+	  PossibleWord pick = null;
+	  var it = moves.iterator(); 
+	  // Loop through words until we are finished with the array or until we hit the random number.
+	  while(it.hasNext() && (counter != random)) {
+		  pick = it.next();
+		  counter++;
+	  }
+	  // If word is empty throw Exception
+	  if(pick  != null ) {
+		  var word = pick.getTile();
+	  }
+	  else {
+		  throw new Exception("Word null Exception");
+	  }
+	  // add tiles 
+	  
+	  
+  }
 }
--- src/backend/ai/HardAI.java
+++ src/backend/ai/HardAI.java
@@ -1,8 +1,12 @@
 package backend.ai;
 
+import java.util.Random;
+
 import backend.basic.Player;
 import backend.basic.ScrabbleBoard;
-
+/*
+ * @author vivanova
+ */
 public class HardAI extends Player {
 
   Brain hardBrain;
@@ -10,5 +14,41 @@ public class HardAI extends Player {
   public HardAI(String name, String color, Playerstatus status, ScrabbleBoard board) {
     super(name, color, status);
     this.hardBrain = new Brain(board);
+}
+  
+  //TODO Play only on my turn Method
+  
+  
+  public void play() throws Exception {
+	  //TODO add a time before we skip turn
+
+	  // Possible moves to make
+	  var moves = hardBrain.getPlayableWords(getRack(), hardBrain.getWordPossibilities());
+	  int points = 0;
+	  PossibleWord pick = null;
+	  // Create an iterator to move thru the TreeSet
+	  var it = moves.iterator(); 
+	  // We loop until the Tree set has a next member. 
+	  while(it.hasNext()) {
+		  // Save current word in Temporary Storage
+		  var temp =  it.next();
+		  // If we haven't picked a word or if we find a word with more points
+		  if(pick == null || points < temp.getPoints()) {
+			  // We save it.
+			  pick = temp;
+		  }
+		  
+	  }
+	  // If our word has been picked we retrieve the tiles
+	  if(pick  != null ) {
+		  var word = pick.getTile();
+		  // TODO send words to Server
+	  }
+	  else {
+		  throw new Exception("Word null Exception");
+	  }
+	  // add tiles 
+	  
+	  
   }
 }
