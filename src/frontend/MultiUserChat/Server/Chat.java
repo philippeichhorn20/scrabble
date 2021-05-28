899936bad305c48a505e690ff3290068ff109f48
Viktoria Milenova Ivanova <vivanova@mail.uni-mannheim.de>
2021-05-28 16:17:28 +0200

    #27 Send messages with Enter Key

--- src/frontend/MultiUserChat/Server/Chat.java
+++ src/frontend/MultiUserChat/Server/Chat.java
@@ -1,11 +1,16 @@
 package frontend.MultiUserChat.Server;
 
+import java.awt.Event;
+
 import backend.basic.GameInformation;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -28,26 +33,45 @@ public class Chat{
 	        textArea = new TextArea();
 	        textArea.setEditable(false);
 	        TextField inputField = new TextField();
+	        
 	        Button btnClose = new Button("Close");
 	        btnClose.setOnAction(e -> window.close());
 	        Button btnSend = new Button("Send");
 	        btnSend.setOnAction(e -> {
 	            String chatMessage = inputField.getText();
-	            GameInformation.getInstance().getClientmatch().sendChatMessage(chatMessage);
-	            inputField.setText("");
+	            if (chatMessage != "") {
+	            	GameInformation.getInstance().getClientmatch().sendChatMessage(chatMessage);
+		            inputField.setText("");
+	            }
 	        });
 	        VBox layout = new VBox(10);
+	        inputField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+				@Override
+				public void handle(KeyEvent ke) {
+				     if (ke.getCode().equals(KeyCode.ENTER))
+				     {
+				       btnSend.fire();
+				     }
+
+					
+				}
+	        	
+	        });
 	        layout.getChildren().addAll(textArea,inputField,btnSend,btnClose);
 	        layout.setAlignment(Pos.TOP_CENTER);
 	        Scene scene = new Scene(layout);
 	        window.setScene(scene);
 	}
-	public  void fillTextArea(String from, String text) {
-		this.textArea.appendText(from+": " + text +"\n" );
+
+	public void fillTextArea(String from, String text) {
+		this.textArea.appendText(from + ": " + text + "\n");
 		System.out.println();
 	}
-	
-    public void display() {
-        window.showAndWait();
-    }
+
+	public void display() {
+		window.showAndWait();
+	}
+
+
 }
