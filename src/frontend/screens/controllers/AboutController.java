2fd0530102ede59639a879fe8201353392e37d13
Nils Holger Bernhard Schaefer <nilschae@mail.uni-mannheim.de>
2021-05-31 02:35:20 +0200

    #29 added styleguide to AboutController

--- src/frontend/screens/controllers/AboutController.java
+++ src/frontend/screens/controllers/AboutController.java
@@ -1,39 +1,35 @@
 package frontend.screens.controllers;
 
+import frontend.Main;
 import java.io.IOException;
 import java.util.ResourceBundle;
-
-import javax.print.DocFlavor.URL;
-
-import frontend.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javax.print.DocFlavor.URL;
 
+/**
+ * Controller for about screen.
+ *
+ * @author vivanova
+ */
 public class AboutController {
 
-		
-		@FXML private Button backButton;
+  @FXML private Button backButton;
 
+  @FXML
+  public void goBack(ActionEvent e) throws IOException {
+    Main m = new Main();
+    m.changeScene("screens/settingsScreen.fxml");
+  }
 
-		@FXML
-		public void goBack(ActionEvent e) throws IOException {
-			Main m = new Main();
-			m.changeScene("screens/settingsScreen.fxml");
-		}	
+  @FXML TextFlow rulesTextFlow;
 
-		@FXML TextFlow rulesTextFlow;  
-		
-		// gets the text from here and adds it to the RuleScreen
-	 public void initialize(URL url, ResourceBundle rb) {
-	    Text text = new Text();
-	    rulesTextFlow.getChildren().add(text);
-	    
-
-		}
-	
+  // gets the text from here and adds it to the RuleScreen
+  public void initialize(URL url, ResourceBundle rb) {
+    Text text = new Text();
+    rulesTextFlow.getChildren().add(text);
+  }
 }
-
-
