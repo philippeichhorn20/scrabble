385d471b9edd2f1660b378fc315e6b8c18ccd668
Nils Holger Bernhard Schaefer <nilschae@mail.uni-mannheim.de>
2021-05-31 02:40:35 +0200

    #29 added styleguide to Chat

--- src/frontend/chat/Chat.java
+++ src/frontend/chat/Chat.java
@@ -21,131 +21,168 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 /**
- * 
- * @author vivanova
+ * A Class for a ingame chat.
  *
+ * @author vivanova
  */
 public class Chat {
-	private TextArea textArea;
-	private Stage window;
-	
-	public Chat(String test) {
-		
-	}
+  private TextArea textArea;
+  private Stage window;
 
-	public Chat() {
-		window = new Stage();
-		window.getIcons().add(new Image("frontend/screens/resources/gameIcon.png"));
-		window.initModality(Modality.WINDOW_MODAL);
-		window.initStyle(StageStyle.UTILITY);
-		window.setTitle("Chat");
-		window.setMinHeight(500);
-		window.setMaxHeight(500);
-		window.setMinWidth(250);
-		window.setMaxWidth(260);
-		textArea = new TextArea();
-		textArea.setEditable(false);
-		textArea.setMaxWidth(220);
-		textArea.setMinHeight(300);
-		TextField inputField = new TextField();
-		inputField.setPrefWidth(220);
-		inputField.setMaxHeight(50);
+  public Chat(String test) {}
 
-		Button btnClose = new Button("Close");
-		btnClose.setOnAction(e -> window.close());
-		Button btnSend = new Button("Send");
-		btnSend.setOnAction(e -> {
-			String chatMessage = inputField.getText();
-			if (chatMessage != "") {
-				GameInformation.getInstance().getClientmatch().sendChatMessage(chatMessage);
-				inputField.setText("");
-			}
-		});
-		VBox layout = new VBox(10);
+  /** A constructor for the chat. */
+  public Chat() {
+    window = new Stage();
+    window.getIcons().add(new Image("frontend/screens/resources/gameIcon.png"));
+    window.initModality(Modality.WINDOW_MODAL);
+    window.initStyle(StageStyle.UTILITY);
+    window.setTitle("Chat");
+    window.setMinHeight(500);
+    window.setMaxHeight(500);
+    window.setMinWidth(250);
+    window.setMaxWidth(260);
+    textArea = new TextArea();
+    textArea.setEditable(false);
+    textArea.setMaxWidth(220);
+    textArea.setMinHeight(300);
+    TextField inputField = new TextField();
+    inputField.setPrefWidth(220);
+    inputField.setMaxHeight(50);
 
-		layout.setPadding(new Insets(10.0d));
-		layout.setBackground(
-				new Background(new BackgroundFill(Color.rgb(0, 0, 0, 0), new CornerRadii(0), new Insets(0))));
+    Button btnClose = new Button("Close");
+    btnClose.setOnAction(e -> window.close());
+    Button btnSend = new Button("Send");
+    btnSend.setOnAction(
+        e -> {
+          String chatMessage = inputField.getText();
+          if (chatMessage != "") {
+            GameInformation.getInstance().getClientmatch().sendChatMessage(chatMessage);
+            inputField.setText("");
+          }
+        });
+    VBox layout = new VBox(10);
 
-		((VBox) layout)
-				.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(0), new Insets(0))));
+    layout.setPadding(new Insets(10.0d));
+    layout.setBackground(
+        new Background(
+            new BackgroundFill(Color.rgb(0, 0, 0, 0), new CornerRadii(0), new Insets(0))));
 
-		btnClose.setMaxWidth(60);
-		btnClose.setMinWidth(60);
-		btnClose.setMaxHeight(50);
-		btnClose.setMinHeight(30);
-		btnSend.setMinWidth(60);
-		btnSend.setMaxWidth(60);
-		btnSend.setMaxHeight(50);
-		btnSend.setMinHeight(30);
-		btnClose.setStyle(".button {\n" + "    -fx-padding: 8 15 15 15;\n"
-				+ "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n" + "    -fx-background-radius: 8;\n"
-				+ "    -fx-background-color:\n"
-				+ "            linear-gradient(from 0% 93% to 0% 100%, #e09c1d 0%, #e09c1d 100%),\n"
-				+ "            #7a7c7d,\n" + "            #9b9d9e,\n"
-				+ "            radial-gradient(center 50% 50%, radius 100%, #e09c1d, #e09c1d);\n"
-				+ "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n"
-				+ "    -fx-font-weight: bold;\n" + "    -fx-font-size: 1.3em;\n" + "}\n" + ".button:hover {\n"
-				+ "    -fx-background-color:\n"
-				+ "            linear-gradient(from 0% 93% to 0% 100%, #d4be94 0%,#d4be94 100%),\n"
-				+ "            #7a7c7d,\n" + "            #9b9d9e,\n"
-				+ "            radial-gradient(center 50% 50%, radius 100%, #d4be94, #d4be94);\n" + "}\n"
-				+ ".button:pressed {\n" + "    -fx-padding: 10 15 13 15;\n"
-				+ "    -fx-background-insets: 2 0 0 0,2 0 3 0, 2 0 4 0, 2 0 5 0;\n" + "}\n" + ".button Text {\n"
-				+ "    -fx-fill: white;\n" + "    -fx-effect: dropshadow( gaussian , #a30000 , 0,0,0,2 );\n" + "}");
-		btnSend.setStyle(".button {\n" + "    -fx-padding: 8 15 15 15;\n"
-				+ "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n" + "    -fx-background-radius: 8;\n"
-				+ "    -fx-background-color:\n"
-				+ "            linear-gradient(from 0% 93% to 0% 100%, #e09c1d 0%, #e09c1d 100%),\n"
-				+ "            #7a7c7d,\n" + "            #9b9d9e,\n"
-				+ "            radial-gradient(center 50% 50%, radius 100%, #e09c1d, #e09c1d);\n"
-				+ "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n"
-				+ "    -fx-font-weight: bold;\n" + "    -fx-font-size: 1.3em;\n" + "}\n" + ".button:hover {\n"
-				+ "    -fx-background-color:\n"
-				+ "            linear-gradient(from 0% 93% to 0% 100%, #d4be94 0%,#d4be94 100%),\n"
-				+ "            #7a7c7d,\n" + "            #9b9d9e,\n"
-				+ "            radial-gradient(center 50% 50%, radius 100%, #d4be94, #d4be94);\n" + "}\n"
-				+ ".button:pressed {\n" + "    -fx-padding: 10 15 13 15;\n"
-				+ "    -fx-background-insets: 2 0 0 0,2 0 3 0, 2 0 4 0, 2 0 5 0;\n" + "}\n" + ".button Text {\n"
-				+ "    -fx-fill: white;\n" + "    -fx-effect: dropshadow( gaussian , #a30000 , 0,0,0,2 );\n" + "}");
-		inputField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+    ((VBox) layout)
+        .setBackground(
+            new Background(new BackgroundFill(Color.WHITE, new CornerRadii(0), new Insets(0))));
 
-			@Override
-			public void handle(KeyEvent ke) {
-				if (ke.getCode().equals(KeyCode.ENTER)) {
-					btnSend.fire();
-				}
+    btnClose.setMaxWidth(60);
+    btnClose.setMinWidth(60);
+    btnClose.setMaxHeight(50);
+    btnClose.setMinHeight(30);
+    btnSend.setMinWidth(60);
+    btnSend.setMaxWidth(60);
+    btnSend.setMaxHeight(50);
+    btnSend.setMinHeight(30);
+    btnClose.setStyle(
+        ".button {\n"
+            + "    -fx-padding: 8 15 15 15;\n"
+            + "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n"
+            + "    -fx-background-radius: 8;\n"
+            + "    -fx-background-color:\n"
+            + "            linear-gradient(from 0% 93% to 0% 100%, #e09c1d 0%, #e09c1d 100%),\n"
+            + "            #7a7c7d,\n"
+            + "            #9b9d9e,\n"
+            + "            radial-gradient(center 50% 50%, radius 100%, #e09c1d, #e09c1d);\n"
+            + "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n"
+            + "    -fx-font-weight: bold;\n"
+            + "    -fx-font-size: 1.3em;\n"
+            + "}\n"
+            + ".button:hover {\n"
+            + "    -fx-background-color:\n"
+            + "            linear-gradient(from 0% 93% to 0% 100%, #d4be94 0%,#d4be94 100%),\n"
+            + "            #7a7c7d,\n"
+            + "            #9b9d9e,\n"
+            + "            radial-gradient(center 50% 50%, radius 100%, #d4be94, #d4be94);\n"
+            + "}\n"
+            + ".button:pressed {\n"
+            + "    -fx-padding: 10 15 13 15;\n"
+            + "    -fx-background-insets: 2 0 0 0,2 0 3 0, 2 0 4 0, 2 0 5 0;\n"
+            + "}\n"
+            + ".button Text {\n"
+            + "    -fx-fill: white;\n"
+            + "    -fx-effect: dropshadow( gaussian , #a30000 , 0,0,0,2 );\n"
+            + "}");
+    btnSend.setStyle(
+        ".button {\n"
+            + "    -fx-padding: 8 15 15 15;\n"
+            + "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n"
+            + "    -fx-background-radius: 8;\n"
+            + "    -fx-background-color:\n"
+            + "            linear-gradient(from 0% 93% to 0% 100%, #e09c1d 0%, #e09c1d 100%),\n"
+            + "            #7a7c7d,\n"
+            + "            #9b9d9e,\n"
+            + "            radial-gradient(center 50% 50%, radius 100%, #e09c1d, #e09c1d);\n"
+            + "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n"
+            + "    -fx-font-weight: bold;\n"
+            + "    -fx-font-size: 1.3em;\n"
+            + "}\n"
+            + ".button:hover {\n"
+            + "    -fx-background-color:\n"
+            + "            linear-gradient(from 0% 93% to 0% 100%, #d4be94 0%,#d4be94 100%),\n"
+            + "            #7a7c7d,\n"
+            + "            #9b9d9e,\n"
+            + "            radial-gradient(center 50% 50%, radius 100%, #d4be94, #d4be94);\n"
+            + "}\n"
+            + ".button:pressed {\n"
+            + "    -fx-padding: 10 15 13 15;\n"
+            + "    -fx-background-insets: 2 0 0 0,2 0 3 0, 2 0 4 0, 2 0 5 0;\n"
+            + "}\n"
+            + ".button Text {\n"
+            + "    -fx-fill: white;\n"
+            + "    -fx-effect: dropshadow( gaussian , #a30000 , 0,0,0,2 );\n"
+            + "}");
+    inputField.setOnKeyPressed(
+        new EventHandler<KeyEvent>() {
 
-			}
+          @Override
+          public void handle(KeyEvent ke) {
+            if (ke.getCode().equals(KeyCode.ENTER)) {
+              btnSend.fire();
+            }
+          }
+        });
+    layout.getChildren().addAll(textArea, inputField, btnSend, btnClose);
+    layout.setAlignment(Pos.TOP_CENTER);
+    Scene scene = new Scene(layout);
+    window.setScene(scene);
+  }
 
-		});
-		layout.getChildren().addAll(textArea, inputField, btnSend, btnClose);
-		layout.setAlignment(Pos.TOP_CENTER);
-		Scene scene = new Scene(layout);
-		window.setScene(scene);
-	}
-	
-	public void emptyTextArea() {
-		this.textArea.clear();
-	}
+  public void emptyTextArea() {
+    this.textArea.clear();
+  }
 
-	public void fillTextArea(String from, String text) {
-		this.textArea.appendText(from.substring(0,1).toUpperCase()+from.substring(1).toLowerCase() + ": " + text + "\n");
-		System.out.println();
-	}
-	public String readOutTextArea() {
-		return textArea.getText();
-	}
-	public void setTextArea() {
-		this.textArea = new TextArea();
-		 
-	}
+  /**
+   * Fills the textArea of the chat.
+   *
+   * @param from who the message was.
+   * @param text which should be filled.
+   */
+  public void fillTextArea(String from, String text) {
+    this.textArea.appendText(
+        from.substring(0, 1).toUpperCase() + from.substring(1).toLowerCase() + ": " + text + "\n");
+    System.out.println();
+  }
 
-	public void display() {
-		window.show();
-	}
-	public void close(){
-		window.close();
-	}
+  public String readOutTextArea() {
+    return textArea.getText();
+  }
 
+  public void setTextArea() {
+    this.textArea = new TextArea();
+  }
+
+  public void display() {
+    window.show();
+  }
+
+  public void close() {
+    window.close();
+  }
 }
