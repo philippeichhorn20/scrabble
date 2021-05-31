ce10babcd84d0ab84ce0899974db58f057834f92
Nils Holger Bernhard Schaefer <nilschae@mail.uni-mannheim.de>
2021-05-31 02:33:46 +0200

    #29 added styleguide to SettingsScreenController

--- src/frontend/screens/controllers/SettingsScreenController.java
+++ src/frontend/screens/controllers/SettingsScreenController.java
@@ -1,9 +1,8 @@
 package frontend.screens.controllers;
 
-import frontend.Main;
-
-import java.io.IOException;
 import backend.basic.GameInformation;
+import frontend.Main;
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -12,70 +11,53 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.media.MediaPlayer;
 
-/*
+/**
+ * controller for the setting screen.
  *
- @author vivanova
- * @version 1.0
+ * @auhtor vivanova
  */
-
-/*
-   This is an empty controller for the settings screen. Here the user should be able to change multiple settings concerning the game.
-   (Ideas: sound, different backgrounds, dictionary used)
-   
-*/
 public class SettingsScreenController {
-	@FXML
-	private Button backButton;
-	@FXML
-	private static Slider volumeKey;
-	@FXML
-	private RadioButton radioButton;
-	@FXML
-	private TextArea textArea;
-	@FXML
-	private Button aboutButton;
+  @FXML private Button backButton;
+  @FXML private static Slider volumeKey;
+  @FXML private RadioButton radioButton;
+  @FXML private TextArea textArea;
+  @FXML private Button aboutButton;
 
-	public static MediaPlayer player;
+  public static MediaPlayer player;
 
-	public void goBack(ActionEvent e) throws IOException {
-		Main m = new Main();
-		m.changeScene("screens/mainMenu.fxml");
-	}
-	
-	public void goAbout(ActionEvent e) throws IOException{
-		Main m = new Main();
-		m.changeScene("screens/about.fxml");
-	}
+  public void goBack(ActionEvent e) throws IOException {
+    Main m = new Main();
+    m.changeScene("screens/mainMenu.fxml");
+  }
 
-	public void radioButton(ActionEvent e) throws IOException {
+  public void goAbout(ActionEvent e) throws IOException {
+    Main m = new Main();
+    m.changeScene("screens/about.fxml");
+  }
 
-		player = GameInformation.getInstance().getMusic().getPlayer();
-		boolean isSelected = radioButton.isSelected();
+  /**
+   * A radio button which activate/deactivate the music.
+   *
+   * @param e an event.
+   * @throws IOException normally don't happen.
+   */
+  public void radioButton(ActionEvent e) throws IOException {
 
-		if (isSelected == true) {
-			player.play();
-		} else {
-			player.pause();
-		}
+    player = GameInformation.getInstance().getMusic().getPlayer();
+    boolean isSelected = radioButton.isSelected();
 
-	}
-	
-	@FXML
-	public void initialize() {
+    if (isSelected == true) {
+      player.play();
+    } else {
+      player.pause();
+    }
+  }
 
-		player = GameInformation.getInstance().getMusic().getPlayer();
-//		player.setVolume(0.3);
-//		volumeKey.setValue(player.getVolume() * 100);
-//		volumeKey.valueProperty().addListener(new InvalidationListener() {
-//
-//			@Override
-//			public void invalidated(Observable observable) {
-//
-//				player.setVolume(volumeKey.getValue() / 100);
-//			}
-//
-//		});
-	}
-	
-	
+  /** initilize setting screen controller. */
+  @FXML
+  public void initialize() {
+
+    player = GameInformation.getInstance().getMusic().getPlayer();
+
+  }
 }
